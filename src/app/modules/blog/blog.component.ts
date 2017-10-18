71bd376997e237c48d4edb5a0772950bfaf9fe01
Reinhold Seiss <reinhold.seiss@gmail.com>
2017-10-18 16:52:00 +0200

    Removed Events tag from menu list

--- src/app/modules/blog/blog.component.ts
+++ src/app/modules/blog/blog.component.ts
@@ -1,32 +1,35 @@
-import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Breadcrumb, BreadcrumbService } from '../../services/breadcrumb.service';
 import { Article, BlogApiService } from '../../services/blog-api.service';
 
-import { Observable } from 'rxjs/Rx';
+import { Observable, Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css'],
 })
-export class BlogComponent implements OnInit {
+export class BlogComponent implements OnInit, OnDestroy {
   private breadcrumbs: Breadcrumb[];
   private articles: Article[];
+  private articlesSub: Subscription;
 
   constructor(private breadcrumbService: BreadcrumbService,
               private blogApiService: BlogApiService) {
     this.breadcrumbs = [{ label: 'Blog', url: '/blog' }]
-    this.blogApiService.getArticles()
-      .subscribe(
-        (articles: Article[]) => this.articles = articles,
-        (error: any) => console.log('Ostrich went wrong here!') // Ostrich method!
-      )
-    // this.articles = this.blogApiService.getArticles();
   }
 
   ngOnInit() {
     this.breadcrumbService.setBreadcrumbs(this.breadcrumbs);
+    this.articlesSub = this.blogApiService.getArticles()
+      .subscribe(
+        (articles: Article[]) => this.articles = articles,
+        (error: any) => console.log('Ostrich went wrong here!') // Ostrich method!
+      );
+  }
 
+  ngOnDestroy() {
+    this.articlesSub.unsubscribe();
   }
 
 }
